c45a03966589be63660795fed31759f9cfee2129
Kevin Monisit <monisitkevin@gmail.com>
2021-06-06 22:35:21 -0400

    Created the grid sketch area

---
+++ main.js
@@ -0,0 +1,18 @@
+
+const length = 500; //in pixels
+
+let numberOfSquares = 16;
+
+let gridContainer = document.querySelector("#grid-container")
+
+gridContainer.style['width'] = `${length}px`;
+gridContainer.style['height'] = `${length}px`;
+gridContainer.style["grid-template-columns"] = `repeat(${numberOfSquares}, 1fr)`;
+gridContainer.style["grid-template-columns"] = `repeat(${numberOfSquares}, 1fr)`;
+
+for(let i = 1; i <= numberOfSquares**2; i++) {
+    let element = document.createElement("div");
+    element.style['border'] = '1px solid grey';
+    element.classList.add("item");
+    gridContainer.appendChild(element);
+}
