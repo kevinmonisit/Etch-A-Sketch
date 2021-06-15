d3e13abe8769771ac3de48a8de2e6ca2f36bbe18
Kevin Monisit <monisitkevin@gmail.com>
2021-06-15 12:42:54 -0400

    Added rainbow color functionality

--- main.js
+++ main.js
@@ -1,38 +1,8 @@
 /* eslint-disable valid-jsdoc */
 /* eslint-disable indent */
 
-const length = 500; // in pixels
 
-const numberOfSquares = 9;
-const color = 'rgb(0,0,0)';
-
-
-
-const gridContainer = document.querySelector('#grid-container');
-
-gridContainer.style['width'] = `${length}px`;
-gridContainer.style['height'] = `${length}px`;
-
-gridContainer.style['grid-template-columns'] =
-                    `repeat(${numberOfSquares}, 1fr)`;
-gridContainer.style['grid-template-columns'] =
-                    `repeat(${numberOfSquares}, 1fr)`;
-
-for (let i = 1; i <= numberOfSquares**2; i++) {
-    const element = document.createElement('div');
-    element.style['border'] = '1px solid grey';
-    element.classList.add('item');
-    gridContainer.appendChild(element);
-}
-
-const tiles = document.querySelectorAll('.item');
-
-// drawing
-tiles.forEach((tile) => {
-    tile.addEventListener('mouseover', () => {
-        tile.style['background-color'] = 'black';
-    });
-});
+// ====================== Button Logic =========================
 
 // Color Toggles
 const colorButtons = document.querySelectorAll('.button-color');
@@ -59,5 +29,58 @@ colorButtons.forEach((button) => {
     button.addEventListener('click', (e) => {
         toggleButton(button);
         e.stopPropagation();
+    });
+});
+
+// ====================== Drawing Grid Area =========================
+
+const length = 500; // in pixels
+
+const numberOfSquares = 9;
+
+const gridContainer = document.querySelector('#grid-container');
+
+gridContainer.style['width'] = `${length}px`;
+gridContainer.style['height'] = `${length}px`;
+
+gridContainer.style['grid-template-columns'] =
+                    `repeat(${numberOfSquares}, 1fr)`;
+gridContainer.style['grid-template-columns'] =
+                    `repeat(${numberOfSquares}, 1fr)`;
+
+for (let i = 1; i <= numberOfSquares**2; i++) {
+    const element = document.createElement('div');
+    element.style['border'] = '1px solid grey';
+    element.classList.add('item');
+    gridContainer.appendChild(element);
+}
+
+const tiles = document.querySelectorAll('.item');
+
+/**
+ * Gives a tile a background color
+ */
+function getColor() {
+    if (currentlyActiveButton.getAttribute('id') == 'black-button') {
+        return 'black';
+    } else {
+        const r = getRandomColorIntensity();
+        const g = getRandomColorIntensity();
+        const b = getRandomColorIntensity();
+        return `rgb(${r},${g},${b}`;
+    }
+}
+/**
+ * Returns color intensity from 0 to 255
+ * @returns {Number} color intensity
+ */
+function getRandomColorIntensity() {
+    return Math.floor(Math.random() * (255));
+}
+
+// drawing
+tiles.forEach((tile) => {
+    tile.addEventListener('mouseover', () => {
+        tile.style['background-color'] = getColor();
     });
 });
