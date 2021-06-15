9ad1283d32163ed2aa85cb65641d0c83dedc23d8
Kevin Monisit <monisitkevin@gmail.com>
2021-06-15 12:31:14 -0400

    Fixed formatting, fixed button toggle

--- main.js
+++ main.js
@@ -1,51 +1,57 @@
+/* eslint-disable valid-jsdoc */
+/* eslint-disable indent */
 
-const length = 500; //in pixels
+const length = 500; // in pixels
 
-let numberOfSquares = 9;
+const numberOfSquares = 9;
+const color = 'rgb(0,0,0)';
 
-let gridContainer = document.querySelector("#grid-container")
+
+
+const gridContainer = document.querySelector('#grid-container');
 
 gridContainer.style['width'] = `${length}px`;
 gridContainer.style['height'] = `${length}px`;
-gridContainer.style["grid-template-columns"] = `repeat(${numberOfSquares}, 1fr)`;
-gridContainer.style["grid-template-columns"] = `repeat(${numberOfSquares}, 1fr)`;
 
-for(let i = 1; i <= numberOfSquares**2; i++) {
-    let element = document.createElement("div");
+gridContainer.style['grid-template-columns'] =
+                    `repeat(${numberOfSquares}, 1fr)`;
+gridContainer.style['grid-template-columns'] =
+                    `repeat(${numberOfSquares}, 1fr)`;
+
+for (let i = 1; i <= numberOfSquares**2; i++) {
+    const element = document.createElement('div');
     element.style['border'] = '1px solid grey';
-    element.classList.add("item");
+    element.classList.add('item');
     gridContainer.appendChild(element);
 }
 
-let tiles = document.querySelectorAll('.item');
+const tiles = document.querySelectorAll('.item');
 
-//drawing
+// drawing
 tiles.forEach((tile) => {
     tile.addEventListener('mouseover', () => {
         tile.style['background-color'] = 'black';
     });
 });
 
-//Color Toggles
-let colorButtons = document.querySelectorAll('.button-color');
+// Color Toggles
+const colorButtons = document.querySelectorAll('.button-color');
 
 let currentlyActiveButton = colorButtons[0];
-function toggleButton(button) {
-    console.log("1 ====");
-    console.log(button);
-    console.log(currentlyActiveButton);
-    console.log(button == currentlyActiveButton);
 
-    if(button == currentlyActiveButton) {
-        if(button == colorButtons[0]) {
+/** Toggles two buttons
+ * @param {node} button
+ */
+function toggleButton(button) {
+    if (button != currentlyActiveButton) {
+        if (button != colorButtons[0]) {
             currentlyActiveButton = colorButtons[1];
         } else {
             currentlyActiveButton = colorButtons[0];
         }
 
-
-        colorButtons[0].classList.toggle("active");
-        colorButtons[1].classList.toggle("active");
+        colorButtons[0].classList.toggle('active');
+        colorButtons[1].classList.toggle('active');
     }
 }
 
