0f4d3d34ce5468118318dffb7833d501074b1426
Kevin Monisit <monisitkevin@gmail.com>
2021-06-15 12:16:24 -0400

    Added/fixed button toggle

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const length = 500; //in pixels
 
-let numberOfSquares = 16;
+let numberOfSquares = 9;
 
 let gridContainer = document.querySelector("#grid-container")
 
@@ -19,8 +19,39 @@ for(let i = 1; i <= numberOfSquares**2; i++) {
 
 let tiles = document.querySelectorAll('.item');
 
+//drawing
 tiles.forEach((tile) => {
     tile.addEventListener('mouseover', () => {
         tile.style['background-color'] = 'black';
+    });
+});
+
+//Color Toggles
+let colorButtons = document.querySelectorAll('.button-color');
+
+let currentlyActiveButton = colorButtons[0];
+function toggleButton(button) {
+    console.log("1 ====");
+    console.log(button);
+    console.log(currentlyActiveButton);
+    console.log(button == currentlyActiveButton);
+
+    if(button == currentlyActiveButton) {
+        if(button == colorButtons[0]) {
+            currentlyActiveButton = colorButtons[1];
+        } else {
+            currentlyActiveButton = colorButtons[0];
+        }
+
+
+        colorButtons[0].classList.toggle("active");
+        colorButtons[1].classList.toggle("active");
+    }
+}
+
+colorButtons.forEach((button) => {
+    button.addEventListener('click', (e) => {
+        toggleButton(button);
+        e.stopPropagation();
     });
 });
